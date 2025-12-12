de443727b89ed21194a8bb16ba78f302c20d7ac7
PayalVerma1 <payalvermaseema@gmail.com>
2025-12-12 23:53:37 +0530

    Modularity improved

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     //unwrap_or_else return result or error
@@ -11,30 +11,8 @@ fn main() {
     });
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
-    if let Err(e)=run(config){
+    if let Err(e)=minigrep::run(config){
       println!("Application error {}",e);
       process::exit(1);
     };
 }
-fn run(config:Config)->Result<(),Box<dyn Error>>{
-
-   //? will automatically return the error type 
-  let contents = fs::read_to_string(config.filename)?;
-    println!("{}", contents);
-    Ok(())
-}
-struct Config {
-    query: String,
-    filename: String,
-}
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config { query, filename })
-    }
-}
-//if function returns a  result then use ? if not then use unwrap_or_else or manual handling
